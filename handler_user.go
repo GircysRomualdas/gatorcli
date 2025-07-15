a13a763f4e3217a81c61df7a2e29afc67fce3869
romas <r2udis@gmail.com>
2025-07-16 02:50:59 +0300

    add db conection

--- handler_user.go
+++ handler_user.go
@@ -1,17 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"log"
+
+	"github.com/GircysRomualdas/gatorcli/internal/database"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) == 0 {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 
-	userName := cmd.Args[0]
-	err := s.config.SetUser(userName)
+	name := cmd.Args[0]
+
+	user, err := s.db.GetUserByName(context.Background(), name)
+	if err != nil {
+		log.Fatalf("error: user %q not found\n", name)
+	}
+
+	err = s.config.SetUser(user.Name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	})
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code == "23505" {
+				log.Fatalf("error: user %q already exists\n", name)
+			}
+		}
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+	fmt.Printf("User %s created successfully!\n", user.Name)
+	s.config.SetUser(user.Name)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
+
 	"github.com/GircysRomualdas/gatorcli/internal/config"
+	"github.com/GircysRomualdas/gatorcli/internal/database"
 )
 
 type state struct {
 	config *config.Config
+	db     *database.Queries
 }
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatal("error reading config: %v", err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
 	programState := &state{
 		config: &cfg,
 	}
-
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
@@ -32,6 +37,15 @@ func main() {
 
 	cmdName := os.Args[1]
 	cmdArgs := os.Args[2:]
+
+	db, err := sql.Open("postgres", programState.config.DbURL)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+	programState.db = dbQueries
 
 	err = cmds.run(programState, command{
 		Name: cmdName,
